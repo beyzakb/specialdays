1cf71ac6ad582112625e50e68b9e47a22b1a2ca6
Beyza Akbulut <94783712+beyzakb@users.noreply.github.com>
2023-12-16 16:16:16 +0300

    specialdays

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import data from './data';
+
 
 function App() {
+
+  const [holidays, setHolidays] = useState(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='container'>
+        <h3>Ulusal Tatil Günleri</h3>
+        {holidays.map((holiday)=>{
+          return(
+              <div className='holiday' key={holiday.id}>
+                <img src={holiday.img} alt='holidays'/>
+                <div>
+                  <p>{holiday.title}</p>  
+                  <p>{holiday.date}</p>  
+                </div>
+                
+              </div>
+            )
+        })}
+        <button onClick={()=>setHolidays([])}>Sil</button>
+
+
+      </div>
+    
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        id:1,
+        title:"Ulusal Egemenlik ve Çocuk Bayramı ",
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/1938_04_23_Cumhuriyet.jpg/1200px-1938_04_23_Cumhuriyet.jpg",
+        date:"23 Nisan"
+    },
+    {
+        id:2,
+        title:"Emek ve Dayanışma Günü ",
+        img:"https://upload.wikimedia.org/wikipedia/tr/thumb/7/7e/1_May%C4%B1s_logosu.jpg/345px-1_May%C4%B1s_logosu.jpg",
+        date:"1 Mayıs"
+    },
+    {
+        id:3,
+        title:"Atatürk' ü Anma Gençlik ve Spor Bayramı",
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Festival_of_Youth_and_Sports%2C_1939%2C_Turkey.jpg/375px-Festival_of_Youth_and_Sports%2C_1939%2C_Turkey.jpg",
+        date:"19 Mayıs"
+    },
+    {
+        id:4,
+        title:"Demokrasi ve Milli Birlik Günü",
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/15_Temmuz_Demokrasi_ve_Milli_Birlik_G%C3%BCn%C3%BC_logo.svg/300px-15_Temmuz_Demokrasi_ve_Milli_Birlik_G%C3%BCn%C3%BC_logo.svg.png",
+        date:"15 Temmuz"
+    },
+    {
+        id:5,
+        title:"Zafer Bayramı",
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Tag_des_Sieges_2.JPG/450px-Tag_des_Sieges_2.JPG",
+        date:"30 Ağustos"
+    },
+    {
+        id:6,
+        title:"Cumhuriyet Bayramı",
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/10th_Anniversary_Speech.jpg/375px-10th_Anniversary_Speech.jpg",
+        date:"29 Ekim"
+    },
+]
+
+
+
+
+
+ 
+ 
+
